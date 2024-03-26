a8a56a8e0e91692d2384d5b2dd2076d066d30bd7
dingjintao <992068393@qq.com>
2024-03-26 15:58:51 +0800

    补充

---
+++ src/views/films/FilmsDetail.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <ul>
+        <li v-for="item in dataList" :key="item" @click="handleClick(item)">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default{
+    name:'FilmsDetail',
+    data(){
+        return{
+            dataList:['111','222','333']
+        }
+    },
+    methods:{
+        handleClick(id){
+            this.$router.push({
+                path:'/detail',
+                query:{
+                    id:id
+                }
+            })
+        }
+    }
+}
+</script>
---
+++ src/views/films/HomeView.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="home">
+    <div class="swiper">
+
+    </div>
+   <ul>
+    <li>
+     <router-link to="/detail" custom v-slot="{navigate,isActive}">
+      <li @click="navigate" :class="isActive?'myactive':''">
+        正在热映--{{isActive}}
+      </li>
+      </router-link>
+    </li>
+    <li>
+     <router-link to="/" custom v-slot="{navigate,isActive}">
+      <li @click="navigate" :class="isActive?'myactive':''">
+        即将上线--{{isActive}}
+      </li>
+      </router-link>
+    </li>
+   </ul>
+   <el-divider></el-divider>
+      <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  data(){
+    return{
+      isActive:false,
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+ul{
+  display: flex;
+  justify-content: center;
+}
+li{
+  list-style: none;
+  margin:0 10px ;
+  height: 30px;
+  background: skyblue;
+  line-height: 30px;
+  border-radius: 10%;
+  font-weight: bolder;
+}
+.home{
+  width: 1200px;
+  border: 1px solid black;
+  margin: 0 auto;
+  padding: 20px;
+  .swiper{
+  height: 200px;
+  background: pink;
+}
+.myactive{
+  color: pink;
+}
+}
+</style>
